2c2baccef14c3d04e82b607567dc6a5d20c2a742
Karim Mohamed <37942217+KarimMohamed20@users.noreply.github.com>
2020-10-12 03:47:34 +0200

    Update websocket.js

--- controllers/ride/websocket.js
+++ controllers/ride/websocket.js
@@ -1,17 +1,29 @@
 var wsAuth = require("../../middleware/wsAuth.js")
 var User = require("../../model/User")
+var Ride = require("../../model/Ride")
 module.exports = function (ride) {
     ride.on("connection", async function (socket) {
-        var id = await wsAuth(socket.handshake.query.token);
 
+        // AUTH
+        var id = await wsAuth(socket.handshake.query.token);
+        var rideId = socket.handshake.query.rideId;
         if (id != 'error') {
-            console.log(id)
+
+            // GET USER
             var user = await User.findById(id);
 
+            // STUDENT OR DRIVER
             if (user.accountType == "Student") {
+                // Connected as a Student
                 console.log("Connected as a Student!")
-            } else if(user.accountType=="Driver") {
+                var ride = await Ride.findOne({_id: rideId, 'students': {$in: [id]}})
+                console.log(ride)
+            } else if (user.accountType == "Driver") {
+                // Connected as a Driver
                 console.log("Connected as a Driver!")
+
+                var ride = await Ride.findOne({_id: rideId, "driver.id": id})
+                console.log(ride)
             }
         } else {
             socket.disconnect()
