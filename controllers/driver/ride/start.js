058dbaed2d1a18e76e0661fcaa280263c4c8d22a
Karim Mohamed <37942217+KarimMohamed20@users.noreply.github.com>
2020-10-28 08:21:14 +0200

    Update start.js

--- controllers/driver/ride/start.js
+++ controllers/driver/ride/start.js
@@ -7,12 +7,19 @@ module.exports = async function (req, res) {
     if (ride == null) {
         res.status(401).json({ message: "No ride found" });
     } else {
-        for (var i = 0; i < ride.studentsObjects.length; i++) {
-            if (ride.started = false) {
-                await sendNotification(ride.studentsObjects[i]['email'], { header: `Captain ${ride.driver.fullName} started the ride.`, body: "Currently you can track this ride" })
-            } else {
-                await sendNotification(ride.studentsObjects[i]['email'], { header: `Captain ${ride.driver.fullName} ended the ride.`, body: "Currently you can't track this ride" })
+        try {
+
+            for (var i = 0; i < ride.studentsObjects.length; i++) {
+                if (ride.started = false) {
+                    await sendNotification(ride.studentsObjects[i]['email'], { header: `Captain ${ride.driver.fullName} started the ride.`, body: "Currently you can track this ride" })
+                } else {
+                    await sendNotification(ride.studentsObjects[i]['email'], { header: `Captain ${ride.driver.fullName} ended the ride.`, body: "Currently you can't track this ride" })
+                }
             }
+        } catch (e) {
+            ride.started = !ride.started;
+            await ride.save()
+            res.json(ride);
         }
         ride.started = !ride.started;
         await ride.save()
