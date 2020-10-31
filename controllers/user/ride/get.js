eda745b35a90c82061fa28793ea390651f265cc4
Karim Mohamed <37942217+KarimMohamed20@users.noreply.github.com>
2020-10-31 17:45:00 +0200

    Handle statusCode in backend

--- controllers/user/ride/get.js
+++ controllers/user/ride/get.js
@@ -6,12 +6,16 @@ module.exports = async function (req, res) {
     //   request.user is getting fetched from Middleware after token authentication
     var user = await User.findById(req.user.id);
     var ride = await Ride.findOne({ _id: user.currentRideId, })
+    if(user.currentRideId != null) {
     console.log(ride.students.includes(req.user.id))
     if (ride.students.includes(req.user.id) == false || ride.students.includes(req.user.id) == null || ride.students.includes(req.user.id) == undefined) {
-        res.status(401).json({ message: "You don't have access to this ride" });
+        res.status(403).json({ message: "You don't have access to this ride" });
     } else if (ride.started != true) {
-        res.json({'message':"Your ride didn't started yet"})
+        res.status(403).json({'message':"Your ride didn't started yet"})
     } else {
+        
         res.json(ride);
+    }} else {
+        res.status(403).json({"message":"You don't have any rides currently"})
     }
 }
